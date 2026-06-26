cfe76f3e30ba3e101f62421549ffc91f91a7a2b5
WWojtanowski <122522090+WWojtanowski@users.noreply.github.com>
2026-06-26 18:28:19 +0200

    Delete src/components/ProfilePage.tsx

--- src/components/ProfilePage.tsx
+++
@@ -1,9 +0,0 @@
-import { Steps, Box, Text } from "@chakra-ui/react";
-
-export default function ProfilePage() {
-  return (
-    <Box as="section" py={28}>
-        <Text>Profile Page</Text>
-    </Box>
-  )
-}
